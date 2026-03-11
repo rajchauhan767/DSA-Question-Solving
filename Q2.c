86014d19a80d53d1f92494e56eb3bc6a79ae6cb8
rajchauhan767 <rajchauha777@gmail.com>
2026-03-11 11:39:20 +0530

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+int a, b, c, limit;
+
+```
+printf("Enter the limit: ");
+scanf("%d", &limit);
+
+for (a = 1; a <= limit; a++) {
+    for (b = 1; b <= limit; b++) {
+        for (c = 1; c <= limit; c++) {
+            if (a*a + b*b == c*c) {
+                printf("%d %d %d\n", a, b, c);
+            }
+        }
+    }
+}
+
+return 0;
+```
+
+}
