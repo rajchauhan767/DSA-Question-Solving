40ff6c135212436e6798a8775be4bdcd5adede7c
rajchauhan767 <rajchauha777@gmail.com>
2026-03-11 15:34:36 +0530

    Add merging of two sorted arrays into nums1

---
+++ Q19.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int nums1[m+n], nums2[n];
+
+    for(int i = 0; i < m; i++)
+        scanf("%d", &nums1[i]);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &nums2[i]);
+
+    int i = m - 1, j = n - 1, k = m + n - 1;
+
+    while(i >= 0 && j >= 0) {
+        if(nums1[i] > nums2[j])
+            nums1[k--] = nums1[i--];
+        else
+            nums1[k--] = nums2[j--];
+    }
+
+    while(j >= 0)
+        nums1[k--] = nums2[j--];
+
+    for(int x = 0; x < m + n; x++)
+        printf("%d ", nums1[x]);
+
+    return 0;
+}
