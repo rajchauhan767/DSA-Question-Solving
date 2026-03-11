9612ca2815d0cdfe8f37b1044298f0184e9a2b1b
rajchauhan767 <rajchauha777@gmail.com>
2026-03-11 11:37:33 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+int num, i, flag = 1;
+
+```
+printf("Enter a number: ");
+scanf("%d", &num);
+
+if (num <= 1) {
+    flag = 0;
+}
+
+for (i = 2; i <= num/2; i++) {
+    if (num % i == 0) {
+        flag = 0;
+        break;
+    }
+}
+
+if (flag == 1)
+    printf("True - %d is a Prime Number", num);
+else
+    printf("False - %d is not a Prime Number", num);
+
+return 0;
+```
+
+}
