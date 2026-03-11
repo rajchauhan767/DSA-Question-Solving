ed4178d8d4f7c7efe3754266bde877e1cd7d3509
rajchauhan767 <rajchauha777@gmail.com>
2026-03-11 15:58:37 +0530

    Add string reversal program in Q30.c
    Implement a program to reverse a string input by the user.

---
+++ Q30.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    for(int i = len - 1; i >= 0; i--)
+        printf("%c", str[i]);
+
+    printf("\n");
+    return 0;
+}
