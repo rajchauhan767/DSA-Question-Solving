1b4cf65ebf5ee0611804d2f57b35f5912f550507
rajchauhan767 <rajchauha777@gmail.com>
2026-03-15 22:49:46 +0530

    Implement linked list with middle node function

---
+++ Q41.C
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void printList(struct Node* head) {
+    printf("[");
+    while (head != NULL) {
+        printf("%d", head->data);
+        if (head->next != NULL)
+            printf(",");
+        head = head->next;
+    }
+    printf("]");
+}
+
+struct Node* middleNode(struct Node* head) {
+    struct Node *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main() {
+    int n, x;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        newNode = createNode(x);
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    struct Node* mid = middleNode(head);
+    printList(mid);
+
+    return 0;
+}
