af626cbeff6766509e8d5f6c14e9372ee41fcaa5
rajchauhan767 <rajchauha777@gmail.com>
2026-03-15 22:39:56 +0530

    Implement linked list reversal

---
+++ Q39.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* reverseList(struct Node* head) {
+    struct Node *prev = NULL, *current = head, *next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
+
+void printList(struct Node* head) {
+    printf("[");
+    while (head != NULL) {
+        printf("%d", head->data);
+        if (head->next != NULL)
+            printf(",");
+        head = head->next;
+    }
+    printf("]");
+}
+
+int main() {
+    int n, x;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        newNode = createNode(x);
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    head = reverseList(head);
+    printList(head);
+
+    return 0;
+}
